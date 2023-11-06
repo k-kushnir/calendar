020f7aff07a5a0db52e2071e5e7aed51c0b489b8
Kostiantyn <shaldon2009@gmail.com>
2023-11-06 10:05:39 +0200

    Practice_3

--- Calendar.py
+++ Calendar.py
@@ -1,15 +1,19 @@
 import datetime
+import PySimpleGUI as sg
 
 
 def print_calendar(year, month):
     try:
+        year = int(year)
+        month = int(month)
         cal = datetime.date(year, month, 1)
     except ValueError:
-        print("Введіть коректні значення року і місяця.")
+        sg.popup_error("Введіть коректні значення року і місяця.")
         return
 
-    print(f"Календар на {cal.strftime('%B')} {year}:")
-    print("Пн Вт Ср Чт Пт Сб Нд")
+    calendar_text = f"Календар на {cal.strftime('%B')} {year}:\n"
+    calendar_text += "{:2s} {:2s} {:2s} {:2s} {:2s} {:2s} {:2s}".format(
+        "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд\n")
 
     # Знайти день тижня першого дня місяця
     start_day = datetime.date(year, month, 1)
@@ -23,16 +27,40 @@ def print_calendar(year, month):
     else:
         days_in_month = (datetime.date(year, month + 1, 1) - start_day).days
 
-    # Друк календаря
+    # Додавання днів до рядка календаря
+    current_line = " " * 3 * offset
     for day in range(1, days_in_month + 1):
-        if day == 1:
-            print(" " * 3 * offset, end="")
-        print(f"{day:2d} ", end="")
+        current_line += f"{day:2d} "
         if (day + offset) % 7 == 0:
-            print()
+            calendar_text += current_line + "\n"
+            current_line = ""
+    return calendar_text
+
+
+def program():
+    layout = [
+        [sg.Text("Введіть рік:"), sg.InputText(key="year")],
+        [sg.Text("Введіть місяць (1-12):"), sg.InputText(key="month")],
+        [sg.Button("Показати календар"), sg.Button("Вийти")],
+        [sg.Output(size=(30, 10), key="output")],
+    ]
+
+    window = sg.Window("Календар", layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == "Вийти":
+            break
+
+        if event == "Показати календар":
+            year = values["year"]
+            month = values["month"]
+            window["output"].update("")
+            window["output"].print(print_calendar(year, month))
+
+    window.close()
 
 
 if __name__ == "__main__":
-    year = int(input("Введіть рік: "))
-    month = int(input("Введіть місяць (1-12): "))
-    print_calendar(year, month)
+    program()
