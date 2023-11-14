a611f9f79d9731f916f240eb45498be43b64294e
Kostiantyn <shaldon2009@gmail.com>
2023-11-14 11:35:18 +0200

    DRY optimisation lab6

--- Calendar.py
+++ Calendar.py
@@ -2,32 +2,24 @@ import datetime
 import PySimpleGUI as sg
 
 
-def print_calendar(year, month):
+def get_calendar_text(year, month):
     try:
         year = int(year)
         month = int(month)
         cal = datetime.date(year, month, 1)
     except ValueError:
         sg.popup_error("Введіть коректні значення року і місяця.")
-        return
+        return None
 
     calendar_text = f"Календар на {cal.strftime('%B')} {year}:\n"
     calendar_text += "{:2s} {:2s} {:2s} {:2s} {:2s} {:2s} {:2s}".format(
         "Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд\n")
 
-    # Знайти день тижня першого дня місяця
     start_day = datetime.date(year, month, 1)
-
-    # Визначити відступ перед першим днем
     offset = start_day.weekday()
+    days_in_month = (datetime.date(year + 1, 1, 1) if month ==
+                     12 else datetime.date(year, month + 1, 1) - start_day).days
 
-    # Визначити кількість днів у місяці
-    if month == 12:
-        days_in_month = (datetime.date(year + 1, 1, 1) - start_day).days
-    else:
-        days_in_month = (datetime.date(year, month + 1, 1) - start_day).days
-
-    # Додавання днів до рядка календаря
     current_line = " " * 3 * offset
     for day in range(1, days_in_month + 1):
         current_line += f"{day:2d} "
@@ -57,7 +49,10 @@ def program():
             year = values["year"]
             month = values["month"]
             window["output"].update("")
-            window["output"].print(print_calendar(year, month))
+
+            calendar_text = get_calendar_text(year, month)
+            if calendar_text:
+                window["output"].update(calendar_text)
 
     window.close()
 
